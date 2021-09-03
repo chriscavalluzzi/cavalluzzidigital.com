10cf75d30cc767763e95182bee3d6a1c8b33fb82
Chris Cavalluzzi <chriscavalluzzi@gmail.com>
2021-09-03 01:26:40 -0600

    Convert titlebar data to php variables

--- index.php
+++ index.php
@@ -1,2 +1,7 @@
-<?php include 'pages/components/header.php' ?>
-<?php include 'pages/components/footer.php' ?>
+<?php
+$titlebar_video_file = "test.mp4";
+$titlebar_title_text = "THIS SITE WAS BUILT FROM SCRATCH";
+$titlebar_subtitle_text = "with PHP, HTML5, CSS, JavaScript, and jQuery";
+include 'pages/components/header.php';
+?>
+<?php include 'pages/components/footer.php'; ?>
--- pages/components/header.php
+++ pages/components/header.php
@@ -44,12 +44,12 @@
         </navbar>
         <titlebar class="titlebarheight">
             <video autoplay muted loop class="titlebarvid titlebarheight">
-                <source src="/videos/test.mp4" type="video/mp4">
+                <source src="/videos/<?php echo $titlebar_video_file; ?>" type="video/mp4">
             </video>
             <titlebarcontentbox class="titlebarheight">
                 <div class="centered">
-                    <div class="font-bold" style="font-size: xx-large;">THIS SITE WAS BUILT FROM SCRATCH</div>
-                    <div style="font-size: x-large;">with PHP, HTML5, CSS, JavaScript, and jQuery</div>
+                    <div class="font-bold" style="font-size: xxx-large;"><?php echo $titlebar_title_text; ?></div>
+                    <div style="font-size: x-large;"><?php echo $titlebar_subtitle_text; ?></div>
                 </div>
             </titlebarcontentbox>
         </titlebar>
