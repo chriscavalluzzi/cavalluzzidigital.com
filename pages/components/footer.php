e1600e8bc17dabe67eba45ac719336eec87fe985
Chris Cavalluzzi <chriscavalluzzi@gmail.com>
2021-09-20 01:23:39 -0600

    Add social links to footer

--- pages/components/footer.php
+++ pages/components/footer.php
@@ -1,5 +1,7 @@
             </body-content>
             <footer>
+                <a href="https://github.com/chriscavalluzzi" target="_blank" rel="noopener noreferrer" class="footer-social-link social-link-github"></a>
+                <a href="https://www.linkedin.com/in/chris-cavalluzzi" target="_blank" rel="noopener noreferrer" class="footer-social-link social-link-linkedin"></a>
                 <footer-company-info>
                     Cavalluzzi Digital, LLC
                 </footer-company-info>
