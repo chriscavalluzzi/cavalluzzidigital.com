cae7d2e3b0ee55aa3ac57955ba0407c8df01483f
Chris Cavalluzzi <chriscavalluzzi@gmail.com>
2021-09-26 17:46:53 -0600

    Add footer logo

--- pages/components/footer.php
+++ pages/components/footer.php
@@ -3,10 +3,13 @@
             <footer>
                 <a href="https://github.com/chriscavalluzzi" target="_blank" rel="noopener noreferrer" class="footer-social-link social-link-github"></a>
                 <a href="https://www.linkedin.com/in/chris-cavalluzzi" target="_blank" rel="noopener noreferrer" class="footer-social-link social-link-linkedin"></a>
-                <footer-company-info>
-                    Cavalluzzi Digital, LLC<br>
-                    <a href="mailto:contact@cavalluzzidigital.com">contact@cavalluzzidigital.com</a>
-                </footer-company-info>
+                <footer-company-info-container>
+                    <footer-company-text>
+                        Cavalluzzi Digital, LLC<br>
+                        <a class="footer-company-email-link" href="mailto:contact@cavalluzzidigital.com">contact@cavalluzzidigital.com</a>
+                    </footer-company-text>
+                    <footer-company-logo></footer-company-logo>
+                </footer-company-info-container>
             </footer>
         </body-container>
     </body>
