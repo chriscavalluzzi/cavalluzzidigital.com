26bdca4fe19c260364d8daa283672a41f1000b8e
Chris Cavalluzzi <chriscavalluzzi@gmail.com>
2021-09-26 22:16:02 -0600

    Fix blank space after footer

--- pages/components/footer.php
+++ pages/components/footer.php
@@ -3,13 +3,11 @@
             <footer>
                 <a href="https://github.com/chriscavalluzzi" target="_blank" rel="noopener noreferrer" class="footer-social-link social-link-github"></a>
                 <a href="https://www.linkedin.com/in/chris-cavalluzzi" target="_blank" rel="noopener noreferrer" class="footer-social-link social-link-linkedin"></a>
-                <footer-company-info-container>
-                    <footer-company-text>
-                        Cavalluzzi Digital, LLC<br>
-                        <a class="footer-company-email-link" href="mailto:contact@cavalluzzidigital.com">contact@cavalluzzidigital.com</a>
-                    </footer-company-text>
-                    <footer-company-logo></footer-company-logo>
-                </footer-company-info-container>
+                <footer-company-logo></footer-company-logo>
+                <footer-company-text>
+                    Cavalluzzi Digital, LLC<br>
+                    <a class="footer-company-email-link" href="mailto:contact@cavalluzzidigital.com">contact@cavalluzzidigital.com</a>
+                </footer-company-text>
             </footer>
         </body-container>
     </body>
