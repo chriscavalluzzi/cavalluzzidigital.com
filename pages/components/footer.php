97c5730ce8a6be83d4d0005f15fd48daa2b9c96a
Chris Cavalluzzi <chriscavalluzzi@gmail.com>
2021-12-07 03:25:08 -0700

    Anchor footer to bottom of viewport if not enough content

--- pages/components/footer.php
+++ pages/components/footer.php
@@ -1,6 +1,17 @@
 
-            </body-content>
-            <footer>
+                </body-content>
+            </body-container>
+<?php
+    if ($videobar_enabled) {
+        echo <<<HEREDOC
+                    <footer class="body-container-videobar-offset">
+        HEREDOC;
+    } else {
+        echo <<<HEREDOC
+                    <footer class="body-container-navbar-offset">
+        HEREDOC;
+    }
+?>
                 <a href="https://github.com/chriscavalluzzi" target="_blank" rel="noopener noreferrer" class="footer-social-link social-link-github"></a>
                 <a href="https://www.linkedin.com/in/chris-cavalluzzi" target="_blank" rel="noopener noreferrer" class="footer-social-link social-link-linkedin"></a>
                 <footer-company-logo></footer-company-logo>
@@ -9,6 +20,6 @@
                     <a class="footer-company-email-link" href="mailto:contact@cavalluzzidigital.com">contact@cavalluzzidigital.com</a>
                 </footer-company-text>
             </footer>
-        </body-container>
+        </page-container>
     </body>
 </html>
--- pages/components/header.php
+++ pages/components/header.php
@@ -45,81 +45,82 @@
 ?>
     </head>
     <body class="font-regular">
-        <navbar id="navbar">
-            <navbar-row>
-                <navbar-bookend></navbar-bookend>
-                <navbar-item class="navbar-title navbar-button" onclick="navbarNavigate('')">
-                    <centered-text-container class="font-bold">CHRIS<navbar-dynamicbreak></navbar-dynamicbreak>CAVALLUZZI</centered-text-container>
-                </navbar-item>
-                <navbar-item class="navbar-item-persistent navbar-divider-container"><navbar-divider></navbar-divider></navbar-item>
-                <navbar-item class="navbar-button navbar-dropdown-button" onclick="navbarToggleMenu()">
-                    <centered-text-container>MENU<span class="icon-angle-down" style="margin-left: 2px;"></span></centered-text-container>
-                    <navbar-dropdown>
-                        <navbar-item class="navbar-button navbar-dropdown-item<?php navbar_highlight('3D'); ?>" onclick="navbarNavigate('3D')">
-                            <centered-text-container>3D MODELING<navbar-dynamicbreak></navbar-dynamicbreak>AND ANIMATION</centered-text-container>
-                        </navbar-item>
-                        <navbar-item class="navbar-button navbar-dropdown-item<?php navbar_highlight('editing'); ?>" onclick="navbarNavigate('editing')">
-                            <centered-text-container>VIDEO AND<navbar-dynamicbreak></navbar-dynamicbreak>IMAGE EDITING</centered-text-container>
-                        </navbar-item>
-                        <navbar-item class="navbar-button navbar-dropdown-item<?php navbar_highlight('software'); ?>" onclick="navbarNavigate('software')">
-                            <centered-text-container>SOFTWARE<navbar-dynamicbreak></navbar-dynamicbreak>DEVELOPMENT</centered-text-container>
-                        </navbar-item>
-                        <navbar-item class="navbar-button navbar-dropdown-item<?php navbar_highlight('web'); ?>" onclick="navbarNavigate('web')">
-                            <centered-text-container>WEBSITE<navbar-dynamicbreak></navbar-dynamicbreak>DEVELOPMENT</centered-text-container>
-                        </navbar-item>
-                        <navbar-item class="navbar-button navbar-dropdown-item<?php navbar_highlight('about'); ?>" onclick="navbarNavigate('about')">
-                            <centered-text-container>MY STORY</centered-text-container>
-                        </navbar-item>
-                        <navbar-item class="navbar-button navbar-dropdown-item<?php navbar_highlight('contact'); ?>" onclick="navbarNavigate('contact')">
-                            <centered-text-container>CONTACT</centered-text-container>
-                        </navbar-item>
-                    </navbar-dropdown>
-                    <navbar-dropdown-cancel-overlay onclick="navbarDisableMenu(); stopOnclickPropagation(arguments[0]);"></navbar-dropdown-cancel-overlay>
-                </navbar-item>
-                <navbar-item class="navbar-button<?php navbar_highlight('3D'); ?>" onclick="navbarNavigate('3D')">
-                    <centered-text-container>3D MODELING<navbar-dynamicbreak></navbar-dynamicbreak>AND ANIMATION</centered-text-container>
-                </navbar-item>
-                <navbar-item class="navbar-button<?php navbar_highlight('editing'); ?>" onclick="navbarNavigate('editing')">
-                    <centered-text-container>VIDEO AND<navbar-dynamicbreak></navbar-dynamicbreak>IMAGE EDITING</centered-text-container>
-                </navbar-item>
-                <navbar-item class="navbar-button<?php navbar_highlight('software'); ?>" onclick="navbarNavigate('software')">
-                    <centered-text-container>SOFTWARE<navbar-dynamicbreak></navbar-dynamicbreak>DEVELOPMENT</centered-text-container>
-                </navbar-item>
-                <navbar-item class="navbar-button<?php navbar_highlight('web'); ?>" onclick="navbarNavigate('web')">
-                    <centered-text-container>WEBSITE<navbar-dynamicbreak></navbar-dynamicbreak>DEVELOPMENT</centered-text-container>
-                </navbar-item>
-                <navbar-item class="navbar-button<?php navbar_highlight('about'); ?>" onclick="navbarNavigate('about')">
-                    <centered-text-container>MY<navbar-dynamicbreak></navbar-dynamicbreak>STORY</centered-text-container>
-                </navbar-item>
-                <navbar-space></navbar-space>
-                <navbar-item class="navbar-button navbar-item-float-opposite<?php navbar_highlight('contact'); ?>" onclick="navbarNavigate('contact')">
-                    <centered-text-container>CONTACT</centered-text-container>
-                </navbar-item>
-                <navbar-bookend></navbar-bookend>
-            </navbar-row>
+        <page-container>
+            <navbar id="navbar">
+                <navbar-row>
+                    <navbar-bookend></navbar-bookend>
+                    <navbar-item class="navbar-title navbar-button" onclick="navbarNavigate('')">
+                        <centered-text-container class="font-bold">CHRIS<navbar-dynamicbreak></navbar-dynamicbreak>CAVALLUZZI</centered-text-container>
+                    </navbar-item>
+                    <navbar-item class="navbar-item-persistent navbar-divider-container"><navbar-divider></navbar-divider></navbar-item>
+                    <navbar-item class="navbar-button navbar-dropdown-button" onclick="navbarToggleMenu()">
+                        <centered-text-container>MENU<span class="icon-angle-down" style="margin-left: 2px;"></span></centered-text-container>
+                        <navbar-dropdown>
+                            <navbar-item class="navbar-button navbar-dropdown-item<?php navbar_highlight('3D'); ?>" onclick="navbarNavigate('3D')">
+                                <centered-text-container>3D MODELING<navbar-dynamicbreak></navbar-dynamicbreak>AND ANIMATION</centered-text-container>
+                            </navbar-item>
+                            <navbar-item class="navbar-button navbar-dropdown-item<?php navbar_highlight('editing'); ?>" onclick="navbarNavigate('editing')">
+                                <centered-text-container>VIDEO AND<navbar-dynamicbreak></navbar-dynamicbreak>IMAGE EDITING</centered-text-container>
+                            </navbar-item>
+                            <navbar-item class="navbar-button navbar-dropdown-item<?php navbar_highlight('software'); ?>" onclick="navbarNavigate('software')">
+                                <centered-text-container>SOFTWARE<navbar-dynamicbreak></navbar-dynamicbreak>DEVELOPMENT</centered-text-container>
+                            </navbar-item>
+                            <navbar-item class="navbar-button navbar-dropdown-item<?php navbar_highlight('web'); ?>" onclick="navbarNavigate('web')">
+                                <centered-text-container>WEBSITE<navbar-dynamicbreak></navbar-dynamicbreak>DEVELOPMENT</centered-text-container>
+                            </navbar-item>
+                            <navbar-item class="navbar-button navbar-dropdown-item<?php navbar_highlight('about'); ?>" onclick="navbarNavigate('about')">
+                                <centered-text-container>MY STORY</centered-text-container>
+                            </navbar-item>
+                            <navbar-item class="navbar-button navbar-dropdown-item<?php navbar_highlight('contact'); ?>" onclick="navbarNavigate('contact')">
+                                <centered-text-container>CONTACT</centered-text-container>
+                            </navbar-item>
+                        </navbar-dropdown>
+                        <navbar-dropdown-cancel-overlay onclick="navbarDisableMenu(); stopOnclickPropagation(arguments[0]);"></navbar-dropdown-cancel-overlay>
+                    </navbar-item>
+                    <navbar-item class="navbar-button<?php navbar_highlight('3D'); ?>" onclick="navbarNavigate('3D')">
+                        <centered-text-container>3D MODELING<navbar-dynamicbreak></navbar-dynamicbreak>AND ANIMATION</centered-text-container>
+                    </navbar-item>
+                    <navbar-item class="navbar-button<?php navbar_highlight('editing'); ?>" onclick="navbarNavigate('editing')">
+                        <centered-text-container>VIDEO AND<navbar-dynamicbreak></navbar-dynamicbreak>IMAGE EDITING</centered-text-container>
+                    </navbar-item>
+                    <navbar-item class="navbar-button<?php navbar_highlight('software'); ?>" onclick="navbarNavigate('software')">
+                        <centered-text-container>SOFTWARE<navbar-dynamicbreak></navbar-dynamicbreak>DEVELOPMENT</centered-text-container>
+                    </navbar-item>
+                    <navbar-item class="navbar-button<?php navbar_highlight('web'); ?>" onclick="navbarNavigate('web')">
+                        <centered-text-container>WEBSITE<navbar-dynamicbreak></navbar-dynamicbreak>DEVELOPMENT</centered-text-container>
+                    </navbar-item>
+                    <navbar-item class="navbar-button<?php navbar_highlight('about'); ?>" onclick="navbarNavigate('about')">
+                        <centered-text-container>MY<navbar-dynamicbreak></navbar-dynamicbreak>STORY</centered-text-container>
+                    </navbar-item>
+                    <navbar-space></navbar-space>
+                    <navbar-item class="navbar-button navbar-item-float-opposite<?php navbar_highlight('contact'); ?>" onclick="navbarNavigate('contact')">
+                        <centered-text-container>CONTACT</centered-text-container>
+                    </navbar-item>
+                    <navbar-bookend></navbar-bookend>
+                </navbar-row>
 <?php
     if ($videobar_enabled) {
         echo <<<HEREDOC
-                    <videobar class="videobar-height" id="videobar-container">
-                        <video autoplay muted loop playsinline class="videobar-height">
-                            <source src="/videos/$videobar_video_file" type="video/mp4">
-                        </video>
-                        <videobar-contentbox>
-                            <centered-text-container>
-                                <videobar-title class="font-bold" id="videobar-title">$videobar_title_text</videobar-title>
-                                <videobar-subtitle id="videobar-subtitle">$videobar_subtitle_text</videobar-subtitle>
-                            </centered-text-container>
-                        </videobar-contentbox>
-                    </videobar>
-                </navbar>
-                <body-container class="body-container-videobar-offset">
-                    <body-content>
+                        <videobar class="videobar-height" id="videobar-container">
+                            <video autoplay muted loop playsinline class="videobar-height">
+                                <source src="/videos/$videobar_video_file" type="video/mp4">
+                            </video>
+                            <videobar-contentbox>
+                                <centered-text-container>
+                                    <videobar-title class="font-bold" id="videobar-title">$videobar_title_text</videobar-title>
+                                    <videobar-subtitle id="videobar-subtitle">$videobar_subtitle_text</videobar-subtitle>
+                                </centered-text-container>
+                            </videobar-contentbox>
+                        </videobar>
+                    </navbar>
+                    <body-container class="body-container-videobar-offset">
+                        <body-content>
         HEREDOC;
     } else {
         echo <<<HEREDOC
-                </navbar>
-                <body-container class="body-container-navbar-offset">
-                    <body-content>
+                    </navbar>
+                    <body-container class="body-container-navbar-offset">
+                        <body-content>
         HEREDOC;
     }
 ?>
