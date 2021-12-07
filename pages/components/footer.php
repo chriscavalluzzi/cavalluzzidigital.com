babb0b3b9d001870add5917fd4899644676ae625
Chris Cavalluzzi <chriscavalluzzi@gmail.com>
2021-12-07 04:35:49 -0700

    Fix footer anchoring position issues on short pages

--- pages/components/footer.php
+++ pages/components/footer.php
@@ -1,17 +1,7 @@
 
                 </body-content>
             </body-container>
-<?php
-    if ($videobar_enabled) {
-        echo <<<HEREDOC
-                    <footer class="body-container-videobar-offset">
-        HEREDOC;
-    } else {
-        echo <<<HEREDOC
-                    <footer class="body-container-navbar-offset">
-        HEREDOC;
-    }
-?>
+            <footer>
                 <a href="https://github.com/chriscavalluzzi" target="_blank" rel="noopener noreferrer" class="footer-social-link social-link-github"></a>
                 <a href="https://www.linkedin.com/in/chris-cavalluzzi" target="_blank" rel="noopener noreferrer" class="footer-social-link social-link-linkedin"></a>
                 <footer-company-logo></footer-company-logo>
