585e526cd9bc5479ee90e69228eae5bc063fe2bf
Chris Cavalluzzi <chriscavalluzzi@gmail.com>
2021-12-17 09:29:57 -0700

    Update GitHub footer link for public repo

--- pages/components/footer.php
+++ pages/components/footer.php
@@ -1,7 +1,7 @@
                 </body-content>
             </body-container>
             <footer>
-                <a href="https://github.com/chriscavalluzzi" target="_blank" rel="noopener noreferrer" class="footer-social-link social-link-github"></a>
+                <a href="https://github.com/chriscavalluzzi/cavalluzzidigital.com" target="_blank" rel="noopener noreferrer" class="footer-social-link social-link-github"></a>
                 <a href="https://www.linkedin.com/in/chris-cavalluzzi" target="_blank" rel="noopener noreferrer" class="footer-social-link social-link-linkedin"></a>
                 <footer-company-logo></footer-company-logo>
                 <footer-company-text>
