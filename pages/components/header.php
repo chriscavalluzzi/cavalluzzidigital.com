4f480ff57e3ad3971e3e7a2657911be71e113724
Chris Cavalluzzi <chriscavalluzzi@gmail.com>
2022-04-03 03:26:06 -0600

    Remove noindex directive

--- pages/components/header.php
+++ pages/components/header.php
@@ -18,7 +18,6 @@
 <html lang="en">
     <head>
         <meta charset="utf-8"/>
-        <meta name="robots" content="noindex">
         <meta name="description" content="<?php echo($page_description); ?>">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title><?php echo($page_title); ?></title>
